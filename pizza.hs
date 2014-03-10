99b53a2a7dc818662f8f461edbddcae6f60b4584
Danny Riordan <dannyriordan@gmail.com>
2014-03-10 12:22:27 +0000

    Update pizza.hs

--- pizza.hs
+++ pizza.hs
@@ -1,7 +1,7 @@
 Veg1  = [onion, mushroom, sweetcorn]
-Veg2  = []
+Veg2  = [extra_cheese]
 Meat3 = [pepperoni, chicken, meatballs]
-Meat4 = [pepperoni, chilli beef, jalapenos, chilli flakes]
+Meat4 = [pepperoni, chilli_beef, jalapenos, chilli_flakes]
 Meat5 = [ham, sweetcorn, bacon]
 
 -- Note that we can get the extra-large pizza on the "Any Pizza €9.99" deal
